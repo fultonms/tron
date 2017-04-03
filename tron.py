a5d5000778b5e19cd29c50f98912f83fe879cc4d
Michael Fulton <fultonms@clarkson.edu>
2017-04-03 15:55:00 -0400

    quick updates to tron.py

--- tron.py
+++ tron.py
@@ -10,6 +10,9 @@ class Point(object):
       self.coords = np.array(coords)
       self.dim = len(coords)
       self.label = label
+
+   def __str__(self):
+      return "Point: " + str(self.coords) + " " + str(self.label)
    
 class Tron(object):
    def __init__(self, dim, points=[]):
@@ -18,16 +21,18 @@ class Tron(object):
       self.v = np.zeros(dim)
 
    def learn(self):
-      for i in xrange(100):
-         p = choice(self.points)
+      for p in self.points:
          predicted = np.dot(self.v, p.coords)
          error = p.label - unit_step(predicted)
-         self.v += 0.2 * error * p.coords
+         self.v += error * p.coords
          
    def predict(self, points):
       for p in points:
          predicted = np.dot(p.coords, self.v)
          print unit_step(predicted)
+   
+   def addPoint(self, point):
+      self.points.append(point)
 
 if __name__ == '__main__':   
    training = [
@@ -39,7 +44,3 @@ if __name__ == '__main__':
    tron = Tron(3, training)
    tron.learn()
    tron.predict(tron.points)
-   
-
-else:
-   print "ehhh..."
